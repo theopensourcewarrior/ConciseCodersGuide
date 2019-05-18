f0355f6646268010dfc0b17ce42b178f16d08fc7
William <mrwwhitney@gmail.com>
2019-05-18 13:42:36 -0500

    Added map stream map grouping

---
+++ src/main/java/Stream/StreamMapGrouping.java
@@ -0,0 +1,60 @@
+package Stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamMapGrouping
+{
+
+    private static class Student
+    {
+
+        private final String name;
+
+        private final int age;
+
+        public Student(String name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        final List<Student> students = new ArrayList<>();
+        students.add(new Student("Kim", 20));
+        students.add(new Student("Mike", 20));
+        students.add(new Student("Joe", 21));
+        students.add(new Student("Beth", 22));
+
+        final Map<Integer, List<Student>> studentsByAge
+                = students.stream().collect(Collectors.groupingBy(Student::getAge));
+
+        for (int age : studentsByAge.keySet())
+        {
+            System.out.println("Students of Age: " + age);
+
+            final List<Student> studentsForAge = studentsByAge.get(age);
+
+            for (Student currentStudent : studentsForAge)
+            {
+                System.out.println(currentStudent.name);
+            }
+
+            System.out.println();
+        }
+    }
+}
